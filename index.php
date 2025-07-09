e54e5dd56e9e362ab4936e8a6cec5c6ff24847bd
米诺 <1213235865@qq.com>
2025-07-09 09:07:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-/**
- * @author    校长bloG <1213235865@qq.com>
- * @github    https://github.com/vpsaz/XzPlayer
- */
-
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET');
+
+$config_file = __DIR__ . '/config.php';
+$conf = include($config_file);
 
 $url = $_POST['url'] ?? $_GET['url'] ?? '';
 $titleParam = $_POST['title'] ?? $_GET['title'] ?? '';
@@ -22,18 +20,79 @@ if (!preg_match('/^https?:\/\//i', $url)) {
     exit;
 }
 
-header('Content-type: text/html;charset=utf-8');
+$baiapiApiKey = '1393c25907a85e0549621b829389a548'; // M3U8广告过滤 的密钥
+$baiapikeywords = ''; // 自定义广告关键词 (多个使用","分隔)
+$baiapiUrl = 'https://baiapi.cn/api/m3u8gl/?apikey=' . $baiapiApiKey . '&url=' . urlencode($url) . '&keywords=' . $baiapikeywords;
 
-$safe_url = htmlspecialchars($url, ENT_QUOTES);
+$cacheDir = __DIR__ . '/Cache';
+if (!is_dir($cacheDir)) {
+    mkdir($cacheDir, 0755, true);
+}
+
+$cacheFileName = md5($url) . '.m3u8';
+$cacheFilePath = $cacheDir . '/' . $cacheFileName;
+
+$cacheExpire = 60;
+
+if (!file_exists($cacheFilePath) || filemtime($cacheFilePath) < (time() - $cacheExpire)) {
+    $maxAttempts = 2;
+    $success = false;
+
+    for ($attempt = 1; $attempt <= $maxAttempts; $attempt++) {
+        $content = @file_get_contents($baiapiUrl);
+        if ($content !== false && strlen($content) > 10) {
+            file_put_contents($cacheFilePath, $content);
+            $success = true;
+            break;
+        }
+    }
+
+    if (!$success) {
+        header('Content-type: application/json;charset=utf-8');
+        echo json_encode(['code' => 500, 'msg' => '无法从 BaiAPI 获取播放地址，请稍后再试'], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        exit;
+    }
+}
+
+$now = time();
+foreach (glob(__DIR__ . '/Cache/*.m3u8') as $file) {
+    if (is_file($file) && filemtime($file) < $now - $cacheExpire) {
+        @unlink($file);
+    }
+}
+
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://';
+$host = $_SERVER['HTTP_HOST'];
+$scriptDir = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
+if (is_file($cacheFilePath)) {
+    $safe_url = $protocol . $host . $scriptDir . '/Cache/' . $cacheFileName;
+} else {
+    header('Content-type: application/json;charset=utf-8');
+    echo json_encode(['code' => 500, 'msg' => '缓存文件不存在，无法播放'], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
+$safe_url = htmlspecialchars($safe_url, ENT_QUOTES);
+
+$currentDomain = $_SERVER['HTTP_HOST'];
+$refererDomain = parse_url($_SERVER['HTTP_REFERER'] ?? '', PHP_URL_HOST);
+
+if ($refererDomain === $currentDomain && !empty($titleParam)) {
+    $title = '正在播放：' . htmlspecialchars($titleParam, ENT_QUOTES);
+} else {
+    $title = htmlspecialchars($conf['site_title'], ENT_QUOTES) . ' - 播放器';
+}
 ?>
 <!DOCTYPE html>
 <html lang="zh">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BaiAPI - 播放器</title>
-    <link rel="shortcut icon" href="https://pic1.imgdb.cn/item/6812e03558cb8da5c8d5d3c3.png" type="image/x-icon">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo $title; ?></title>
+    <meta name="description" content="<?php echo $conf['description']; ?>" />
+    <meta name="keywords" content="<?php echo $conf['keywords']; ?>" />
+    <link rel="shortcut icon" href="https://pic1.imgdb.cn/item/6812e03558cb8da5c8d5d3c3.png" type="image/x-icon" />
     <script src="https://cdn.jsdelivr.net/npm/hls.js@1.4.8/dist/hls.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/flv.js@1.6.2/dist/flv.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/dashjs@4.7.1/dist/dash.all.min.js"></script>
@@ -48,7 +107,6 @@ $safe_url = htmlspecialchars($url, ENT_QUOTES);
             overflow: hidden;
             background-color: #000;
         }
-
         #video {
             width: 100%;
             height: 100%;
